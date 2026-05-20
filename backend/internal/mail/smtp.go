d7ac87ca63506226eaccfdd50feca6484671b110
MxianD <caiqings@andrew.cmu.edu>
2026-05-20 16:33:42 -0400

    email

--- backend/internal/mail/smtp.go
+++ backend/internal/mail/smtp.go
@@ -1,6 +1,7 @@
 package mail
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/smtp"
 	"strings"
@@ -34,5 +35,49 @@ func SendPlain(cfg *config.Config, to, subject, body string) error {
 	if strings.TrimSpace(cfg.SMTPUser) != "" {
 		auth = smtp.PlainAuth("", cfg.SMTPUser, cfg.SMTPPassword, host)
 	}
+	if port == 465 || port == 994 {
+		return sendMailTLS(addr, host, auth, from, []string{to}, msg)
+	}
 	return smtp.SendMail(addr, auth, from, []string{to}, msg)
+}
+
+func sendMailTLS(addr, host string, auth smtp.Auth, from string, to []string, msg []byte) error {
+	conn, err := tls.Dial("tcp", addr, &tls.Config{ServerName: host, MinVersion: tls.VersionTLS12})
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client, err := smtp.NewClient(conn, host)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	if auth != nil {
+		if err := client.Auth(auth); err != nil {
+			return err
+		}
+	}
+	if err := client.Mail(from); err != nil {
+		return err
+	}
+	for _, rcpt := range to {
+		if err := client.Rcpt(rcpt); err != nil {
+			return err
+		}
+	}
+
+	writer, err := client.Data()
+	if err != nil {
+		return err
+	}
+	if _, err := writer.Write(msg); err != nil {
+		_ = writer.Close()
+		return err
+	}
+	if err := writer.Close(); err != nil {
+		return err
+	}
+	return client.Quit()
 }
